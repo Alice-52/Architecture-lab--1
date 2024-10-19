3010bff58f55503bb36575ebc64fb6f741f9b06d
Alice-52 <aalevitskaia@edu.hse.ru>
2024-10-19 20:22:23 +0300

    Красота и исправления

--- laba.sh
+++ laba.sh
@@ -1,126 +1,112 @@
 #!/bin/bash
 
-#Принимаем в качестве аргумента путь к папке
+# Принимаем в качестве аргумента путь к папке
 dir="$1"
 
-#Проверяем существование папки
-#-d - is a directory(-f - is a file)
-if [ -d "$dir" ];
-then
- echo "Folder exists, resuming work..."
+# Проверяем существование папки
+if [ -d "$dir" ]; then
+  echo "Folder exists, resuming work..."
 else
- while [ ! -d "$dir" ];
- do
-  echo "Folder doesn't exists, try again or enter EXIT."
-  read dir
+  while [ ! -d "$dir" ]; do
+    echo "Folder doesn't exist, try again or enter EXIT."
+    read dir
 
-  if [[ "$dir" == "EXIT" ]]; then
-   echo "Exiting the programm.."
-   exit 0
-  fi
-
- done
- echo "Folder exists, resuming work..."
+    if [[ "$dir" == "EXIT" ]]; then
+      echo "Exiting the program..."
+      exit 0
+    fi
+  done
+  echo "Folder exists, resuming work..."
 fi
 
-
-#Создание ограниченной папки - 10MB, потому что так захотелось
-
-#dd - копирование блочных данных с устройства /dev/zero(спец файл - источник  нулевых байтов)
-#в файл образа диска; bs - сколько байт читать и записывать
-#count - скопировать указанное кол-во блоков, размера bs
+# Создание ограниченной папки - 2 ГБ
 dd if=/dev/zero of=limit.img bs=1G count=2
 
-#создаём файловую систему на нашем образе диска
+# Создаём файловую систему на нашем образе диска
 mkfs.ext4 limit.img
 
-#создаём маунт поинт
+# Создаём маунт поинт
 sudo mkdir /mnt/limited_fol
 
-#и примаунтиваем
-#--options loop - используем loop device(ненастоящее устройство-просто файл>#т.к хотим примонтировать файл - образ диска, а не устройство
+# Монтируем образ
 sudo mount -o loop limit.img /mnt/limited_fol
 
-#Выделяем имя папки для создания ссылки
+# Выделяем имя папки для создания символической ссылки
 name="$(basename "$dir")"
 
-#передвигаем нашу директорию в ограниченную папку с помощью move
-sudo mv $dir /mnt/limited_fol
+# Перемещаем нашу директорию в ограниченную папку
+sudo mv "$dir" /mnt/limited_fol
 
-#создаём soft-link(symbolic),чтобы можно было работать из изначального места
-ln -s /mnt/limited_fol/$name $dir
+# Создаём символическую ссылку, чтобы можно было работать из изначального места
+ln -s /mnt/limited_fol/"$name" "$dir"
 
+# Считаем размер папки
+folder_size=$(du -sL "$dir" | cut -f1)
+folder_size=$((folder_size / 2048))  # Преобразуем в мегабайты
 
-#Считаем размер папки
-let "folder_size = $(du -sL $dir | cut -f1) / 1024"
+echo "The size of the folder is, out of 2G we've limited your folder to :)"
+du -shL "$dir"
 
-echo "The size of the folder is, out of 2G we've limited your folder to :) "
-du -shL $dir
-
-#Берём проценты для архивирования
+# Процент заполнения папки
 echo "How full is your folder in percents: $folder_size%"
 echo "What is your preferable threshold of fullness in percents?"
 read border
 
-
-#Архивация
+# Архивация, если размер папки больше порога
 if [ "$folder_size" -gt "$border" ]; then
- echo "Too much memory taken, starting the clearing process..."
-#Сколько файлов убрать 
- echo "How many files do you want us to clear?"
- read amount
+  echo "Too much memory taken, starting the clearing process..."
+  echo "How many files do you want us to clear?"
+  read amount
 
-#Пользователь даёт папку, куда отправить  тары файлов
- echo "Where do you want us to place backups for your files?"
- read b_dir
-#Опять проверка существования папки
- if [ -d "$b_dir" ];
- then
-  echo "Folder exists, resuming work..."
- else
-  while [ ! -d "$b_dir" ];
-  do
-   echo "Folder doesn't exists, try again or enter EXIT."
-   read b_dir
+  echo "Where do you want us to place backups for your files?"
+  read b_dir
 
-   if [[ "$b_dir" == "EXIT" ]]; then
-    echo "Exiting the programm.."
+  # Проверка существования директории для бэкапов
+  if [ -d "$b_dir" ]; then
+    echo "Folder exists, resuming work..."
+  else
+    while [ ! -d "$b_dir" ]; do
+      echo "Folder doesn't exist, try again or enter EXIT."
+      read b_dir
+
+      if [[ "$b_dir" == "EXIT" ]]; then
+        echo "Exiting the program..."
+        exit 0
+      fi
+    done
+    echo "Folder exists, resuming work..."
+  fi
+
+  # Находим старые файлы
+  old_files=$(find "$dir" -type f -printf '%T+ %p\n' | sort | head -n "$amount" | cut -d ' ' -f 2-)
+  echo "LIST: $old_files"
+
+  if [ -z "$old_files" ]; then
+    echo "Archive list is empty"
     exit 0
-   fi
+  fi
 
-  done
-  echo "Folder exists, resuming work..."
- fi
+  # Создаём архив и удаляем старые файлы
+  just_file="$b_dir/backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+  tar -czf "$just_file" "$old_files"
 
-#Показываем старые файлы
- old_files=$(find "$dir" -type f -printf '%T+ %p\n' | sort | head -n "$amount" | cut -d ' ' -f 2-)
- echo "LIST: $old_files"
-
- if [ -z "$old_files" ]; then
- echo "Archive list is empty"
- exit 0
- fi
-
-#Создаём тары из старых файлов и удаляем их
-
- just_file="$b_dir/backup_$(date +%Y%m%d_%H%M%S).tar.gz"
- tar -czf "$just_file" $old_files
- 
- rm $old_files
-
- echo "Archivation done to $just_file and removed from $dir"
-
+  rm "$old_files"
+  echo "Archivation done to $just_file and removed from $dir"
 else
- echo "Your folder is not that full, exiting the programm."
+  echo "Your folder is not that full, exiting the program."
 fi
 
-#Убираем маунт и удаляем образ диска
-#убираем созданную ссылку
+# Убираем созданную символическую ссылку
 sudo rm "$dir"
-#размонтируем
+
+# Удаляем перемещённую папку
+sudo rm -r /mnt/limited_fol/"$name"
+
+# Размонтируем файловую систему
 sudo umount /mnt/limited_fol
-#Удаляем маунт поинт, созданный ранее
-sudo rm -r /mnt/limited_fol/$name
+
+# Удаляем маунт поинт
 sudo rmdir /mnt/limited_fol
-#Удаляем созданный образ диск
+
+# Удаляем созданный образ диска
 rm limit.img

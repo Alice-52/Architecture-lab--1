1993e65f120e998e77a9df344ff26d7642360650
Alice-52 <aalevitskaia@edu.hse.ru>
2024-10-19 19:57:18 +0300

    Ошибки исправила

--- laba.sh
+++ laba.sh
@@ -29,7 +29,7 @@ fi
 #dd - копирование блочных данных с устройства /dev/zero(спец файл - источник  нулевых байтов)
 #в файл образа диска; bs - сколько байт читать и записывать
 #count - скопировать указанное кол-во блоков, размера bs
-dd if=/dev/zero of=limit.img bs=1G count=2
+dd if=/dev/zero of=limit.img bs=G count=2
 
 #создаём файловую систему на нашем образе диска
 mkfs.ext4 limit.img
@@ -41,20 +41,21 @@ sudo mkdir /mnt/limited_fol
 #--options loop - используем loop device(ненастоящее устройство-просто файл>#т.к хотим примонтировать файл - образ диска, а не устройство
 sudo mount -o loop limit.img /mnt/limited_fol
 
+name="$(basename "$dir")"
 
 #передвигаем нашу директорию в ограниченную папку с помощью move
-sudo cp -r $dir /mnt/limited_fol
+sudo mv $dir /mnt/limited_fol
 
 #создаём soft-link(symbolic),чтобы можно было работать из изначального места
-ln -s /mnt/limited_fol $dir
+ln -s /mnt/limited_fol/$name $dir
 #ls -l /mnt/limited_fol
 
 
 #Считаем размер папки
-let "folder_size = $(du -s $dir | cut -f1) / 100"
+let "folder_size = $(du -sL $dir | cut -f1) / 1024"
 
 echo "The size of the folder is, out of 2G we've limited your folder to :) "
-du -sh $dir
+du -shL $dir
 
 #Берём проценты для архивирования
 echo "How full is your folder in percents: $folder_size%"
@@ -72,7 +73,6 @@ if [ "$folder_size" -gt "$border" ]; then
 #Пользователь даёт папку, куда отправить  тары файлов
  echo "Where do you want us to place backups for your files?"
  read b_dir
-
 #Опять проверка существования папки
  if [ -d "$b_dir" ];
  then
@@ -116,7 +116,7 @@ fi
 
 #Убираем маунт и удаляем образ диска
 #убираем созданную ссылку
-#unlink $dir
+sudo rm $dir/$name
 #размонтируем
 sudo umount /mnt/limited_fol
 #Удаляем маунт поинт, созданный ранее
